f200e33fbc9f51dbda9f7216111d98c438d6a53b
Thomas Hunter II <me@thomashunter.name>
2014-10-04 13:20:36 -0700

    background music (disabled for now)

--- public/scripts/world.js
+++ public/scripts/world.js
@@ -4,10 +4,14 @@ var World = function(game) {
 
 World.prototype.preload = function() {
     this.game.stage.backgroundColor = '#1f1d2c';
+
+    game.load.audio('background', 'audio/background.mp3');
 };
 
 World.prototype.create = function() {
+    this.music = game.add.audio('background', 1, true);
 
+    //this.music.play('', 0, 0.1, true);
 };
 
 World.prototype.update = function() {
