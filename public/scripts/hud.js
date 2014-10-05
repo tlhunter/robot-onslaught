e85450c2a90b93cd0e0ed80598e47b97780dc94a
Thomas Hunter II <me@thomashunter.name>
2014-10-05 15:08:07 -0700

    hearts

--- public/scripts/hud.js
+++ public/scripts/hud.js
@@ -4,6 +4,7 @@ var HUD = function(game) {
     this.game = game;
     this.text = {};
     this.hud = null;
+    this.hearts = [];
 };
 
 HUD.prototype.FONT = {
@@ -21,7 +22,7 @@ HUD.prototype.FONT_SMALL = {
 };
 
 HUD.prototype.preload = function() {
-
+    this.game.load.spritesheet('heart', 'images/heart.png', 24, 24);
 };
 
 HUD.prototype.create = function() {
@@ -29,12 +30,25 @@ HUD.prototype.create = function() {
     this.hud.enableBody = false;
     this.hud.fixedToCamera = true;
 
+    var heart;
+    for (var i = 0; i < 5; i++) {
+        // backgrounds
+        heart = this.game.add.sprite(8 + 28 * i, 8, 'heart', 1);
+        this.hud.add(heart);
+
+        // red hearts
+        heart = this.game.add.sprite(8 + 28 * i, 8, 'heart', 0);
+        this.hearts.push(heart);
+        this.hud.add(heart);
+    }
+    //this.icons.dark.exists = false;
+
     //this.text.score = this.game.add.text(16, 16, 'POINTS: 0', this.FONT);
-    this.text.health = this.game.add.text(16, 16, 'HEALTH: 5/5', this.FONT);
-    this.text.debug = this.game.add.text(16, 40, '', this.FONT_SMALL);
+   // this.text.health = this.game.add.text(16, 16, 'HEALTH: 5/5', this.FONT);
+    this.text.debug = this.game.add.text(8, 40, '', this.FONT_SMALL);
 
    // this.hud.add(this.text.score);
-    this.hud.add(this.text.health);
+   // this.hud.add(this.text.health);
     this.hud.add(this.text.debug);
 };
 
@@ -47,5 +61,13 @@ HUD.prototype.update = function() {
     text += "FPS: " + this.game.time.fps + "/60";
 
     this.text.debug.text = text;
-    this.text.health.text = "HEALTH: " + player.health + "/5";
+
+    for (var i = 0; i < 5; i++) {
+        if (i > player.health - 1) {
+            this.hearts[i].exists = false;
+        } else {
+            this.hearts[i].exists = true;
+        }
+    }
+   // this.text.health.text = "HEALTH: " + player.health + "/5";
 };
